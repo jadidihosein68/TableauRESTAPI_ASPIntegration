143e80f7a016939794e9823e4f3155cd6e1c2200
Hossein <jadidi.hosein@gmail.com>
2018-04-09 10:05:21 +0800

    Add API

---
+++ TableauRestAPI/Controllers/Api/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TableauRestAPI.Controllers.Api
+{
+    public class TestController : ApiController
+    {
+
+
+
+        public IHttpActionResult getTest() {
+
+            return Ok("I am fucking working! ");
+        }
+
+    }
+}
--- TableauRestAPI/Global.asax.cs
+++ TableauRestAPI/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using System.Web.Routing;
+
 
 namespace TableauRestAPI
 {
@@ -12,6 +15,7 @@ namespace TableauRestAPI
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
